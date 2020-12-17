387914d7f1d1816b34769ec83a52a667e68305de
Yuriy Lytovchenko <yuri.litovchenko@gmail.com>
2020-12-17 13:51:39 +0200

    Homework 7

--- src/SumAverage.java
+++ src/SumAverage.java
@@ -12,7 +12,10 @@ public class SumAverage {
    */
   public static int sum(int lowerBound, int upperBound) {
     // TODO fill in code here using for loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int i = lowerBound; i <= upperBound; i++)
+      sum += i;
+    return sum;
   }
 
   /**
@@ -23,7 +26,10 @@ public class SumAverage {
    */
   public static int sum(int[] numbers) {
     // TODO fill in code here using for each loop and replace the return statement
-    return -1;
+    int sum = 0;
+    for (int numba : numbers)
+      sum += numba;
+    return sum;
   }
 
   /**
@@ -35,7 +41,15 @@ public class SumAverage {
    */
   public static double average(int lowerBound, int upperBound) {
     // TODO fill in code here using while loop and replace the return statement
-    return -1;
+    int numberOfItems = 0;
+    int sum = 0;
+    for (int i = lowerBound; i <= upperBound; i++) {
+      numberOfItems ++;
+      sum += i;
+    }
+    if (numberOfItems == 0)
+      return 0;
+    return (double)sum / numberOfItems;
   }
 
   /**
@@ -46,6 +60,12 @@ public class SumAverage {
    */
   public static double average(int[] numbers) {
     // TODO fill in code here using do-while loop and replace the return statement
-    return -1;
+    int numberOfItems = numbers.length;
+    int sum = 0;
+    if (numberOfItems == 0)
+      return 0;
+    for (int numba : numbers)
+      sum += numba;
+    return (double)sum / numberOfItems;
   }
 }
